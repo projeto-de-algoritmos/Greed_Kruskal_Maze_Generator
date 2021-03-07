ed0000955444e166e922ac32a529f2ab658fcd69
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-03-07 09:55:54 -0300

    Handle button's events
    Co-authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

--- GeraLabirintos/events.py
+++ GeraLabirintos/events.py
@@ -1,4 +1,6 @@
-from pygame import QUIT
+from pygame import QUIT, MOUSEBUTTONDOWN, MOUSEBUTTONUP
+from pygame import mouse
+from buttonsHelper import buttons_on_click
 
 def treats_event(event) -> bool:
     """treats pygame events
@@ -12,3 +14,7 @@ def treats_event(event) -> bool:
     """
     if event.type == QUIT:
         return True
+    elif event.type == MOUSEBUTTONDOWN:
+        x, y = mouse.get_pos()
+        buttons_on_click(x, y)
+    return False
