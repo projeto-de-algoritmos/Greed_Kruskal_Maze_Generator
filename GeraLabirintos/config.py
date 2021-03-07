10b8fba178632e93e1fa3ec1cb150d609c977633
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-03-07 09:55:20 -0300

    Create button that generate random maze
    Co-authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

--- GeraLabirintos/config.py
+++ GeraLabirintos/config.py
@@ -1,8 +1,9 @@
 import pygame
-from colors import BLACK, STANDARD_COLOR, LIGHTBLUE
+from colors import BLACK, STANDARD_COLOR, LIGHTBLUE, PRIM_COLOR
 
 # Classes
 from Board import Board
+from Button import Button
 
 pygame.init()
 
@@ -15,6 +16,10 @@ pygame.display.set_caption("Gera Labirintos")
 
 # Create an object to help track time
 clock = pygame.time.Clock()
+
+# All buttons
+maze_button = Button(PRIM_COLOR, 1315, 200, 225, 200, "Maze")
+buttons = [maze_button]
 
 # Objects
 board = Board(25, 25, 40, 50, 0, STANDARD_COLOR)
